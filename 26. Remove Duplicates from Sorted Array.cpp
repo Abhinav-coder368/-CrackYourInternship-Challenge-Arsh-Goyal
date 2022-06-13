25ccc182c93fc30c0b8ffe355db9c3fa8a1d592e
Abhinav Gangwar <76556517+Abhinav-coder368@users.noreply.github.com>
2022-06-14 00:03:42 +0530

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 26. Remove Duplicates from Sorted Array.cpp
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -18,3 +18,20 @@ public:
         return nums.size();
     }
 };
+
+
+//TWO POINTER APPOROACH
+//     int removeDuplicates(vector<int>& a) 
+//     {
+//         int n=a.size();
+//         if(n<=1)
+//         return n;
+//         int j=0;
+//         for(int i=0;i<n-1;i++)
+//         {
+//             if(a[i]!=a[i+1])
+//             a[j++]=a[i];
+//         }
+//         a[j++]=a[n-1];
+//         return j;
+//     }
